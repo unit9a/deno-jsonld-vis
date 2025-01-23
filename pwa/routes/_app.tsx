c9617cbf5303a7192d67b286bfe96d7f3a2ebcbe
unit9a <danielo.dfxi@gmail.com>
2025-01-23 11:57:04 +0000

    Test: not working following pwa tutorial from https://ericdavidsmith.com/blog/deno/deno-fresh-pwa-progressive-web-app
    will try https://www.pwabuilder.com/ + https://web.dev/learn/pwa

--- pwa/routes/_app.tsx
+++ pwa/routes/_app.tsx
@@ -1,16 +1,34 @@
 import { type PageProps } from "$fresh/server.ts";
+import { Head } from "https://deno.land/x/fresh@1.1.6/runtime.ts";
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
+      <Head>
+        <link
+          crossorigin="use-credentials"
+          rel="manifest"
+          href="/manifest.webmanifest"
+        />
+
+        <script type="module">
+          import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate/dist/pwa-update.js";
+          const el = document.createElement("pwa-update"); document.body.appendChild(el);
+        </script>
+      </Head>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
         <title>pwa</title>
         <link rel="stylesheet" href="/styles.css" />
+
       </head>
       <body>
         <Component />
       </body>
+      
+
     </html>
   );
 }
