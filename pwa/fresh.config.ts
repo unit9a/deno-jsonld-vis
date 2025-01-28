1f4e29e9446bef6af0788cdefcdb5e79f90e9dc1
unit9a <danielo.dfxi@gmail.com>
2025-01-28 14:11:20 +0000

    renamed files to match file extensions of  example in  https://docs.deno.com/runtime/fundamentals/http_server/#https-support
    ```
    Deno.serve({
      port: 443,
      cert: Deno.readTextFileSync("./cert.pem"),
      key: Deno.readTextFileSync("./key.pem"),
    }, handler);
    ```

--- pwa/fresh.config.ts
+++ pwa/fresh.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "$fresh/server.ts";
 
 
 export default defineConfig({
-  cert: Deno.readTextFileSync("./localhost.crt"),
-  key: Deno.readTextFileSync("./localhost.key")
+  cert: Deno.readTextFileSync("./localhost-cert.pem"),
+  key: Deno.readTextFileSync("./localhost-key.pem")
 });
