8d2334cdbdd5386af8a2defade0f15aa422db5be
unit9a <danielo.dfxi@gmail.com>
2025-01-29 10:38:22 +0000

    following the mozilla tutorial... get errors in chrome but no firefox

--- pwa/fresh.config.ts
+++ pwa/fresh.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "$fresh/server.ts";
 
 
 export default defineConfig({
-  cert: Deno.readTextFileSync("./localhost-cert.pem"),
-  key: Deno.readTextFileSync("./localhost-key.pem")
+    cert: Deno.readTextFileSync("./localhost-cert.pem"),
+    key: Deno.readTextFileSync("./localhost-key.pem")
 });
--- pwa/routes/_app.tsx
+++ pwa/routes/_app.tsx
@@ -1,12 +1,15 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
+
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>pwa</title>
+        <link rel="manifest" href="/app.webmanifest" />
         <link rel="stylesheet" href="/styles.css" />
+        <script src="/init.js"></script>
       </head>
       <body>
         <Component />
